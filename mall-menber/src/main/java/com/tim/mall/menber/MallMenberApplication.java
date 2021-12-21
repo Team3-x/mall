295e04fbfd1c055b39ad146ca7f185a8b0aff83f
team3 <team3_xzh@163.com>
2021-12-21 23:21:52 +0800

    项目初始结构创建

---
+++ mall-menber/src/main/java/com/tim/mall/menber/MallMenberApplication.java
@@ -0,0 +1,13 @@
+package com.tim.mall.menber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMenberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallMenberApplication.class, args);
+    }
+
+}
