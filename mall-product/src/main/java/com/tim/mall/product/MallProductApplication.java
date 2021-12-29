0b8d97901c3302c10aeec17746802fcbcca56a20
team3 <team3_xzh@163.com>
2021-12-29 22:58:02 +0800

    git feat: 增加mybatis-plus配置

--- mall-product/src/main/java/com/tim/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/tim/mall/product/MallProductApplication.java
@@ -1,8 +1,21 @@
 package com.tim.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 整合Mybatis-plus
+ * 1.导入依赖
+ * 2.配置
+ *   1.配置数据源
+ *      1.导入数据路驱动
+ *      2.在application.yml配置数据源相关信息
+ *   2.配置Mybatis-plus
+ *      1.使用@MapperScan
+ *      2.sql映射文件的位置
+ */
+@MapperScan("com.tim.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
